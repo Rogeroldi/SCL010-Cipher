c447d79925ed2690aec84ed49de2e570157e0c1b
Rogeroldi <rossanageroldi19@gmail.com>
2019-06-18 05:22:55 -0400

    cifrando

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,20 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
-    
+  encode: (toEncode, offSet) => {
+    let messageAscii; /*guarda el texto cifrado aun en Ascii*/
+    let messageCode; /*convierte el Ascii en alfabeto normal*/
+    let resultCode = "";
+
+    for (let i = 0; i <= toEncode.length; i++) {
+      let toAscii = toEncode.charCodeAt(i);
+
+      if (toAscii >= 32 && toAscii <= 125) {
+        messageAscii = (toAscii - 32 + offSet) % 94 + 32;
+        messageCode = String.fromCharCode(messageAscii);
+        resultCode += messageCode;
+      }
+    } return resultCode
   },
-  decode: () => {
-    /* Acá va tu código */
+
+  decode: (toDecode, offSet) => {
   }
 };
