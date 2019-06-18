4b2044238991ed4c6207ef4e0802a98298d8cfa2
Rogeroldi <rossanageroldi19@gmail.com>
2019-06-18 00:38:31 -0400

    elementos del dom y arreglo buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-/*Acá va tu código*/
+document.getElementById("codeBtn").addEventListener("click", () => {
+  const toEncode = document.getElementById("textForCode").value;
+  const offSet = Math.abs(parseInt(document.getElementById("codeOffset").value));
+  let result = cipher.encode(toEncode, offSet);
+  document.getElementById("textEncode").innerHTML = result;
+});
