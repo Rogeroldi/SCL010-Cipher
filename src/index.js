975201bf4fec0756b692da82e2059938aec2b8cc
Rogeroldi <rossanageroldi19@gmail.com>
2019-06-23 10:39:46 -0400

    creando constante del codeButton

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-
-const toCodebtn= document.getElementById("codeBtn");
-toCodebtn.addEventListener("click", () => {
-  let toEncode = document.getElementById("textForCode").value;
-  let offSet = parseInt(document.getElementById("offset").value);
-  document.getElementById("textForCode").innerHTML = window.cipher.encode(toEncode, offSet);
-});
-
-// document.getElementById("decodeBtn").addEventListener("click", () => {
-//   const toDecode = document.getElementById("textForCode").value;
-//   const offSet = parseInt(document.getElementById("offset").value);
-//   let result = cipher.encode(toDecode, offSet);
-//   document.getElementById("textForCode").innerHTML = result;
-// });
+const codeButton=document.getElementById("codeButton");
+codeButton.addEventListener("click", ()=>{
+	let offSet=document.getElementById("offset").value;
+	let message=document.getElementById("message").value;
+	
+})
