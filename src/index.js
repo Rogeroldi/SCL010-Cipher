bff20c59f2a819e14851f067cd7ada6c89985b57
Rogeroldi <rossanageroldi19@gmail.com>
2019-06-23 10:46:48 -0400

    completando la constante llamando message y offset desde el html

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const codeButton=document.getElementById("codeButton");
 codeButton.addEventListener("click", ()=>{
 	let offSet=document.getElementById("offset").value;
-	let message=document.getElementById("message").value;
-	
+  let message=document.getElementById("message").value;
+  let result=cipher.encode(offSet,message);
+  document.getElementById(message).value=result;
 })
