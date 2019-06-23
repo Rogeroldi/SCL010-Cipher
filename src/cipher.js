a45620d81eac140e89edaddd28fd70c517874df3
Rogeroldi <rossanageroldi19@gmail.com>
2019-06-23 17:58:47 -0400

    detalles de semantica

--- src/cipher.js
+++ src/cipher.js
@@ -1,93 +1,70 @@
 window.cipher = {
-  encode: (offSet,message) => {
-    let textCipher="";//cada caracer del message
-    let textASCII="";// se ejecutara en el for dando un numero ascii
-    let codeAscii="";//letra que devuelve el texASCII
-    let keyNumber= parseInt(offSet);// let que contiene el numero desplazamiento
+  encode: (offSet, message) => {
+    /* declara variables */
+    let textCipher = "";
+    let textASCII = "";
+    let codeAscii = "";
+    let keyNumber = parseInt(offSet);
 
     for (let i = 0; i < message.length; i++) {
-      textASCII= message.charCodeAt(i);
-      //MAYUSCULAS, si es true true
-      if (textASCII>=65 && textASCII<= 90) {
-      //se calcula el  residuo para acotar los ciclos
-        codeAscii=(textASCII-65+keyNumber)%26+65;
-      //se devuelve una cadena de texto a partir de la secuencia anterior
-        textCipher+= String.fromCharCode(codeAscii);
+      // charCodeAt() es un metodo que devuelve un valor Unicode
+      // del caracter en el indice proporcionado
+      textASCII = message.charCodeAt(i);
+
+      // en el rango de las mayusculas
+      if (textASCII >= 65 && textASCII <= 90) {
+        codeAscii = (textASCII - 65 + keyNumber) % 26 + 65;
+        textCipher += String.fromCharCode(codeAscii);
       }
-      //rango en minuscula 97 a 122
-      else if(textASCII>=97 && textASCII <=122) {
-        codeAscii=(textASCII-97+keyNumber)%26+97;
-        textCipher+=String.fromCharCode(codeAscii);
+
+      // rango en minuscula 97 a 122
+      else if (textASCII >= 97 && textASCII <= 122) {
+        codeAscii = (textASCII - 97 + keyNumber) % 26 + 97;
+        textCipher += String.fromCharCode(codeAscii);
       }
+
+      // pasa por el rango de  numeros y caracteres especiales entre 32 y 64
+      else if (textASCII >= 32 && textASCII <= 64) {
+        // aplicando formula michille en rango de caracter especiales
+        codeAscii = (textASCII - 32 + keyNumber) % 33 + 32;
+        // fromCharCode()crea cadena con valores unicode
+        textCipher += String.fromCharCode(codeAscii);
+      }
+
+
     }
     return textCipher;
+
   },
-  decode: (offSet,message) => {
-  let textCipher="";
-  let textASCII="";
-  let codeAscii="";
-  let keyNumber=parseInt(offSet);
-  for(let i=0; i<message.length; i++){
-    textASCII=message.charCodeAt(i);
-    // en el rango de las mayusculas de 65 a 90
-     if (textASCII>=65 && textASCII <=90) {
-      // para descifrar el tope esta en 90
-      codeAscii=(textASCII-90-keyNumber)%26+90;
-      textCipher+=String.fromCharCode(codeAscii);
+  decode: (offSet, message) => {
+    let textCipher = "";
+    let textASCII = "";
+    let codeAscii = "";
+    let keyNumber = parseInt(offSet);
+
+    for (let i = 0; i < message.length; i++) {
+      textASCII = message.charCodeAt(i);
+
+      // en el rango de las mayusculas de 65 a 90
+      if (textASCII >= 65 && textASCII <= 90) {
+        // para descifrar el tope esta en 90
+        codeAscii = (textASCII - 90 - keyNumber) % 26 + 90;
+        textCipher += String.fromCharCode(codeAscii);
+      }
+      // rango de minusculas 97 a 122
+      else if (textASCII >= 97 && textASCII < 122) {
+        codeAscii = (textASCII - 122 - keyNumber) % 26 + 122;
+        textCipher += String.fromCharCode(codeAscii);
+      }
+      // números y caracteres especiales
+      else if (textASCII >= 32 && textASCII <= 64) {
+        codeAscii = (textASCII - 64 - keyNumber) % 33 + 64;
+        textCipher += String.fromCharCode(codeAscii);
+      }
+
+
     }
-     // rango de minusculas 97 a 122
-     else if(textASCII>=97 && textASCII<122){
-      codeAscii=(textASCII-122-keyNumber)%26+122;
-      textCipher+=String.fromCharCode(codeAscii);
-     }
-     
-   }
-
-//};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
-//   messageascii;
-//   let messageDecode;""
-//   for (let i = 0; i <= toDecode.length; i++) {//charCodeAt es un metodo que devuelve un valor unicode  del carácter en el índice especificado.
-//     let messageascii = toDecode.charCodeAt(i);
-//     //cifrando mayúsculas
-//     if (messageascii >= 65 && messageascii <= 90) {
-//       messageascii = (messageascii - 65 + offSet) % 26 + 65;
-//       messageDeCode = String.fromCharCode(messageascii);
-//       resultCode += messagedeCode;
-//       //cifrando minusculas 32 a 64
-//     if (messageascii >= 97 && messageascii <= 122) {
-//         messageascii = (messageascii - 97 + offSet) % 26 + 97;
-//         messageDeCode = String.fromCharCode(messageascii);
-//         resultCode += messageDeCode;
-//     //cifrando caracteres especiales 97 a 122
-//     if (messageascii >= 32 && messageascii <= 64) {
-//       messageascii = (messageascii - 32 + offSet) % 26 + 32;
-//       messageDeCode = String.fromCharCode(messageascii);
-//       resultCode += messageDeCode;
-
-// };return resultCode;
-//     };
-//   }
+    return textCipher;
+    /* Acá va tu código */
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const codeButton=document.getElementById("codeButton");
-codeButton.addEventListener("click", ()=>{
-	let offSet=document.getElementById("offset").value;
-  let message=document.getElementById("message").value;
-  let result=cipher.encode(offSet,message);
-  document.getElementById("message").value=result;
+const codeButton = document.getElementById("codeButton");
+codeButton.addEventListener("click", () => {
+  let offSet = document.getElementById("offset").value;
+  let message = document.getElementById("message").value;
+  let result = cipher.encode(offSet, message);
+  document.getElementById("message").value = result;
 })
-const decodeButton=document.getElementById("decodeButton");
-decodeButton.addEventListener("click", ()=>{
-	let offSet=document.getElementById("offset").value;
-	let message=document.getElementById("message").value;
-	let result=cipher.decode(offSet,message);
-	document.getElementById("message").value=result;
-} )
 
+const decodeButton = document.getElementById("decodeButton");
+decodeButton.addEventListener("click", () => {
+  let offSet = document.getElementById("offset").value;
+  let message = document.getElementById("message").value;
+  let result = cipher.decode(offSet, message);
+  document.getElementById("message").value = result;
+})
